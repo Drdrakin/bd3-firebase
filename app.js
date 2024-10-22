48aa46611499575d0e88746f96b0f3784aa0079b
Guilherme Alves <drdrakino@gmail.com>
2024-10-22 19:49:53 -0300

    feat: Criação e exclusão

--- app.js
+++ app.js
@@ -5,14 +5,34 @@ function renderList(doc){
     let li = document.createElement('li');
     let autor = document.createElement('span');
     let titulo = document.createElement('span');
+    let excluir = document.createElement('div'); 
 
-    autor.textContent = doc.autor;
-    titulo.textContent = doc.titulo;
+    data = doc.data();
+
+    autor.textContent = data.autor;
+    titulo.textContent = data.titulo;
+    excluir.textContent = 'X';
+
+    li.setAttribute('data-id', doc.titulo);
 
     li.appendChild(titulo);
     li.appendChild(autor);
 
     listBook.appendChild(li);
+
+    //INSERE O ELEMENTO DE TEXT PARA A EXCLUSÃO
+    li.appendChild(excluir);
+
+    /* TRATA A AÇÃO DE CLIQUE NO BOTÃO X PARA EXCLUSÃO DO ARQUIVO */
+    excluir.addEventListener('click', (event)=>{
+        event.stopPropagation();
+
+        let id = event.target.parentElement.getAttribute('data-id');
+
+        db.collection('libri-books').doc(id).delete()
+            .then(()=>{window.location.reload()})
+
+    });
 }
 
 db.collection("libri-collection")
@@ -20,7 +40,7 @@ db.collection("libri-collection")
     .then((snapshot) => {
         snapshot.docs.forEach(
             doc => {
-                renderList(doc.data())
+                renderList(doc)
             }
         )
     })
@@ -29,5 +49,14 @@ const form = document.querySelector('#add-book-form')
 
 form.addEventListener('submit', (event)=>{
     event.preventDefault();
-    alert('Formulário show')
+
+    db.collection("libri-collection").add({
+        autor: form.autor.value,
+        titulo: form.titulo.value
+    }).then(()=>{
+        form.autor.value = '';
+        form.titulo.value = '';
+        window.location.reload();
+    });
 });
+
